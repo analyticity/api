4d685aace1ccbbe025af085c73ac17d8211242fa
Magdaléna Ondrušková <48593629+MagdalenaOndruskova@users.noreply.github.com>
2026-04-14 11:42:19 +0000

    local dev

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ app.add_middleware(LoggingMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["http://localhost:5173"], # Frontend URL - local development
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
