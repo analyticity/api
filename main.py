283c50eed03ea8aa7f58e08c0f0af17d1caa5fb7
--global <maximus27rus@gmail.com>
2026-05-10 01:14:28 +0200

    reverted origins

--- main.py
+++ main.py
@@ -30,11 +30,6 @@ app = FastAPI(
 
 app.add_middleware(LoggingMiddleware)
 
-origins = [
-    "http://localhost:5173", # Example: React/Next.js frontend
-    "http://127.0.0.1:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
